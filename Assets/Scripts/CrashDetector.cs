e947b1b12a4f86aba53aa65e876a5a826caa037d
Rus <punyarit07@gmail.com>
2023-01-08 20:19:11 +0700

    finishing the game

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,12 +7,17 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float InvokeDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
-
+    [SerializeField] AudioClip crashAudio;
+    bool hasCrashed = false;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        if (other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
+            // find the playercontroller class (cs file) then call DisableControls method
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashAudio);
             Invoke("ReloadScene", InvokeDelay);
         }
     }
